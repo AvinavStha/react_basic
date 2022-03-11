6a9bf22da4aa07d45f884c00652e3bc827d72589
AvinavStha <avi.shrestha.775@gmail.com>
2022-03-11 15:33:48 +0545

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,65 @@
-import './App.css';
-import Button from './components/Button';
+import { Warning } from "@material-ui/icons";
+import "./App.css";
+import { Button } from "./components/Button";
+
+function clickMe() {
+  alert("You clicked me");
+} 
+function clickWarning() {
+  console.warn("warning");
+} 
 
 function App() {
   return (
     <div className="App">
-        <Button name="Addition"/>
+      <Button
+        onClick={clickMe}
+        type="button"
+        buttonStyle="btn--primary--solid"
+        buttonSize="btn--medium"
+      >
+        Primary
+      </Button>
+      <Button
+        onClick={clickWarning}
+        type="button"
+        buttonStyle="btn--warning--solid"
+        buttonSize="btn--large"
+      >
+        Warning
+      </Button>
+      <Button
+        onClick={clickMe}
+        type="button"
+        buttonStyle="btn--danger--solid"
+        buttonSize="btn--medium"
+        
+      >
+        Danger
+      </Button>
+      <Button
+        onClick={clickMe}
+        type="button"
+        buttonStyle="btn--rounded--solid"
+        buttonSize="btn--medium"
+      >
+        Rounded Corner
+      </Button>
+      <Button
+        type="button"
+        buttonStyle="btn--transparent--solid"
+        buttonSize="btn--medium"
+        disabled
+      >
+        Transparent
+      </Button>
+      <Button
+        type="button"
+        buttonStyle="btn--boxShadow--solid"
+        buttonSize="btn--medium"
+      >
+        Box-Shadow
+      </Button>
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,66 +1,103 @@
 import React from "react";
 import "./button.css";
 import { ArrowDownward } from "@material-ui/icons";
-import {Info} from "@material-ui/icons";
+import { Info } from "@material-ui/icons";
 
-function clickMe() {
-  alert("You clicked me");
-}
 
-const Button = (props) => {
+
+const STYLES = [
+    "btn--primary--solid",
+    "btn--warning--solid",
+    "btn--danger--solid",
+    "btn--rounded--solid",
+    "btn--transparent--solid",
+    "btn--boxShadow--solid"
+
+
+];
+const SIZES = [
+    "btn--medium",
+    "btn--large"
+];
+
+export const Button = ({
+  children,
+  type,
+  onClick,
+  buttonStyle,
+  buttonSize,
+}) => {
+     /* what it basically does is call the sizes array and the value that is demanded and if not send the first array element*/
+    const checkButtonStyle = STYLES.includes(buttonStyle) 
+    ? buttonStyle
+     : STYLES[0];
+     /* what it basically does is call the styles array and the value that is demanded and if not send the first array element*/
+     const checkButtonSize = SIZES.includes(buttonSize) /*includes: it basically checks if the value that we pass is included or not in the size array*/
+    ? buttonSize
+     : STYLES[0];
+     /*ternary operator basically checks 1= true or false check garne 2 = return if true 3 = return if false */
+
   return (
-    <>
-      <div className="Button_Types">
-        <button className="AddButton" onClick={clickMe}>
-          {props.name}
-        </button>
-      </div>
-      <div className="Button_Types">
-        <button className="MinusButton" onClick={clickMe}>
-          Minus
-        </button>
-      </div>
-      <div className="Button_Types">
-        <button className="AddButton" disabled>
-          Disable
-        </button>
-      </div>
-      <div className="Button_Types">
-        <a href="http://avinavstha.github.io/AviPortfolio">
-          <button className="AddButton">Link</button>
-        </a>
-      </div>
-      <div className="Button_Types">
-        <button className="DownloadButton">
-          <ArrowDownward
-            style={{
-              height: "30px",
-              width: "30px",
-            }}
-          />
-          Download
-        </button>
-        <button className="DownloadButton">
-          <ArrowDownward />
-        </button>
-      </div>
-      <div className="Button_Types">
-        <button className="informationButton">
-        <Info />
-        </button>
-      </div>
-      <div className="Button_Types">
-        <button className="TransparentButton" onClick={clickMe}>
-          transparent
-        </button>
-      </div>
-      <div className="Button_Types">
-        <button className="RoundedCornerButton" onClick={clickMe}>
-          Rounded Corner Button
-        </button>
-      </div>
-    </>
-  );
+    <button className={`btn ${checkButtonStyle} ${checkButtonSize}`} onClick={onClick} type={type}>
+      {children}
+    </button>
+  ); //template string
 };
 
-export default Button;
+// const Button = (props) => {
+//   return (
+//     <>
+//       <div className="Button_Types">
+//         <button className="AddButton" onClick={clickMe}>
+//           {props.name}
+//         </button>
+//       </div>
+//       <div className="Button_Types">
+//         <button className="MinusButton" onClick={clickMe}>
+//           Minus
+//         </button>
+//       </div>
+//       <div className="Button_Types">
+//         <button className="AddButton" disabled>
+//           Disable
+//         </button>
+//       </div>
+//       <div className="Button_Types">
+//         <a href="http://avinavstha.github.io/AviPortfolio">
+//           <button className="AddButton">Link</button>
+//         </a>
+//       </div>
+//       <div className="Button_Types">
+//         <button className="DownloadButton">
+//           <ArrowDownward
+//             style={{
+//               height: "30px",
+//               width: "30px",
+//             }}
+//           />
+//           Download
+//         </button>
+//         <button className="DownloadButton">
+//           <ArrowDownward />
+//         </button>
+//       </div>
+//       <div className="Button_Types">
+//         <button className="informationButton">
+//           <Info />
+//         </button>
+//       </div>
+//       <div className="Button_Types">
+//         <button className="TransparentButton" onClick={clickMe}>
+//           transparent
+//         </button>
+//       </div>
+//       <div className="Button_Types">
+//         <button className="RoundedCornerButton" onClick={clickMe}>
+//           Rounded Corner Button
+//         </button>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Button;
